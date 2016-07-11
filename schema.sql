e4537ed5b77af3896b976eb391db6ef6589b986a
Eliot Whalan <ewhal@pantsu.cat>
2016-07-11 14:18:01 +1000

    Add tasks schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,13 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `tasks` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `title` longtext,
+  `task` longtext,
+  `created` DATETIME,
+  `duedate` DATETIME,
+  `email` VARCHAR(320),
+  PRIMARY KEY (`id`)
+);
+
