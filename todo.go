6a53a3fe53ad4db55d4b6421fea3480060f5facf
Eliot Whalan <ewhal@pantsu.cat>
2016-07-11 11:00:24 +1000

    Add logouthandler

--- todo.go
+++ todo.go
@@ -64,6 +64,14 @@ func registerHandler(w http.responsewriter, r *http.request) {
 }
 
 func logoutHandler(w http.responsewriter, r *http.request) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, cookie)
+	http.Redirect(w, r, "/", 301)
 
 }
 
